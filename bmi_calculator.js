0cf0d347695faea7bd90feca9e24b9ac42a1ccec
Benits <66729989+projectorb@users.noreply.github.com>
2022-11-30 14:36:39 +0800

    Manual Testing for BMI Calculator

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -7,24 +7,41 @@ function getBMI(heigth, weight)
 
   if (bmi < 18.5)
   {
-    console.log('Under Weigth')
     return 'Under Weigth';
   } else if (bmi > 18.5 && bmi < 24.9)
   {
-    console.log('Normal')
     return 'Normal';
   } else if (bmi > 25 && bmi < 29.9)
   {
-    console.log('Over Weigth');
     return 'Over Weigth';
   } else {
-    console.log('Obese');
     return 'Obese';
   }
 
 }
 
-getBMI(1.2, 40)
+function testBMI()
+{
+  var result = getBMI(1.6, 55);
+  var expected = 'Normal';
 
+  console.assert(result === expected, 'Test Failed result should be Normal');
+  
+  result = getBMI(1, 55);
+  expected = 'Obese';
 
-module.exports = getBMI
+  console.assert(result === expected, 'Test Failed result should be Obese');
+
+  result = getBMI(5.4, 30);
+  expected = 'Under Weigth';
+
+  console.assert(result === expected, 'Test Failed result should be Under Weigth');
+
+  result = getBMI(1.2, 40);
+  expected = 'Over Weigth';
+
+  console.assert(result === expected, 'Test Failed result should be Over Weigth');
+  
+}
+
+testBMI();
